b6f82114457d88f5068cd807041dbc9d62a56f96
Dog-Egg <Dog-Egg@users.noreply.github.com>
2024-05-22 00:37:32 +0000

    deploy: 5433f68c5bac35155abc18fb90c2c2c65a56f850

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev20240513161747',
+    VERSION: '0.1.dev20240522003609',
     LANGUAGE: 'zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
