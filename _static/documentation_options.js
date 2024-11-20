dfa77c31ecfe8750cf2855379a14de660b6e1129
Dog-Egg <Dog-Egg@users.noreply.github.com>
2024-11-20 05:17:20 +0000

    deploy: ea6903afcdb187e00f79c4af110b0fc552560f9c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev20241117123648',
+    VERSION: '0.1.dev20241120051650',
     LANGUAGE: 'zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
