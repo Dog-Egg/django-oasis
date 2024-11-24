232db8e8e8d442ed3b8aaac500163489c75ed0cb
Lee <294622946@qq.com>
2024-11-24 11:18:13 +0800

    Update docs

--- docs/samples/upload_file/single_file.py
+++ docs/samples/upload_file/single_file.py
@@ -1,22 +1,17 @@
 from django_oasis import schema
 from django_oasis.core import Resource
-from django_oasis.parameter import FormData
+from django_oasis.parameter import FormItem
 
 
 @Resource("/upload/single")
 class UploadAPI:
     def post(
         self,
-        data=FormData(
-            {
-                "file": schema.File(),
-            }
-        ),
+        file=FormItem(schema.File()),
     ):
         ...
 
         from django.core.files.uploadedfile import InMemoryUploadedFile
 
-        file = data["file"]
         assert file.__class__ is InMemoryUploadedFile, file.__class__
         return file.read()
