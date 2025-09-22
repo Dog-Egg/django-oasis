fc1cdaa7bd6fdf9a9baa402b96699f7e654cf7b5
Lee <294622946@qq.com>
2025-09-22 14:01:49 +0800

    Refactor: simplify parameter handling in apply_signature and introduce S class for decorator creation

--- openapi/signatures.py
+++ openapi/signatures.py
@@ -4,7 +4,7 @@ import typing
 
 import zangar as z
 
-from .basic import Parameter, Path, Query
+from .basic import Path, Query
 
 T = typing.TypeVar("T")
 P = typing.TypeVar("P")
@@ -14,59 +14,46 @@ class _MISSING:
     pass
 
 
-def _inject_keyword_default(kwname: str, default):
-    def decorator(func):
-        if inspect.iscoroutinefunction(func):
-
-            async def wrapper(*args, **kwargs):  # type: ignore
-                if kwname not in kwargs:
-                    kwargs[kwname] = default
-                return await func(*args, **kwargs)
-
-        else:
-
-            def wrapper(*args, **kwargs):
-                if kwname not in kwargs:
-                    kwargs[kwname] = default
-                return func(*args, **kwargs)
-
-        wrapper = functools.wraps(func)(wrapper)
-        return wrapper
-
-    return decorator
-
-
 def apply_signature(func):
     sign = inspect.signature(func)
     for param in reversed(list(sign.parameters.values())):
         name = param.name
-        if isinstance(param.default, SParameter):
-            decorator = param.default.create_parameter(name)
-            func = decorator(func)
-            if (
-                isinstance(param.default, SParameterWithDefault)
-                and param.default.py_default is not _MISSING
-            ):
-                func = _inject_keyword_default(name, param.default.py_default)(func)
+        if isinstance(param.default, S):
+            s = param.default
+            func = s(name)(func)
     return func
 
 
-class SParameter:
-    def __init__(self, cls: type[Parameter], /, **kwargs):
-        self.cls = cls
-        self.kwargs = kwargs
+class S:
+    """
+    等待 `apply_signature` 解析，使用函数签名来创建一个新的装饰器函数。
 
-    def create_parameter(self, kwname: str):
-        return self.cls(
-            kwname,
-            **self.kwargs,
-        )
+    @param creator: 一个可调用对象，接受一个参数 `name`，返回一个装饰器函数。
+    @param default: 默认值，如果提供了这个参数，那么在调用函数时，如果参数没有提供，就会使用这个默认值。
+    """
 
+    def __init__(
+        self,
+        creator: typing.Callable[[str], typing.Callable],
+        /,
+        *,
+        default: typing.Any = _MISSING,
+    ):
+        self.__creator = creator
+        self.__default = default
 
-class SParameterWithDefault(SParameter):
-    def __init__(self, cls: type[Parameter], py_default, /, **kwargs):
-        super().__init__(cls, **kwargs)
-        self.py_default = py_default
+    def __call__(self, name: str):
+        def decorator(func):
+            @self.__creator(name)
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                if name not in kwargs:
+                    kwargs[name] = self.__default
+                return func(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
 
 
 def s_query(
@@ -74,11 +61,17 @@ def s_query(
 ) -> T | P:
     return typing.cast(
         T | P,
-        SParameterWithDefault(
-            Query, py_default, schema=schema, **kwargs, required=py_default is _MISSING
+        S(
+            lambda name: Query(
+                name, schema=schema, **kwargs, required=py_default is _MISSING
+            ),
+            default=py_default,
         ),
     )
 
 
 def s_path(*, schema: z.Schema[T], **kwargs) -> T:
-    return typing.cast(T, SParameter(Path, schema=schema, **kwargs, required=True))
+    return typing.cast(
+        T,
+        S(lambda name: Path(name, schema=schema, **kwargs), default=_MISSING),
+    )
